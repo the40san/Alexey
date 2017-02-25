25bfa119a32f26f67606b4d030de741e28865d99
Masashi AKISUE <masashi.akisue@aktsk.jp>
2017-02-25 15:32:05 +0900

    Add Title screen

--- Assets/Scripts/Manager/GameSuperior.cs
+++ Assets/Scripts/Manager/GameSuperior.cs
@@ -7,19 +7,19 @@ public class GameSuperior : MonoBehaviour {
 	private Map map;
 
 	void Start () {
+		this.inputController = Spawner.SpawnObject("InputController", this.gameObject).GetComponent<InputController>();
+		this.map = Spawner.SpawnObject("Map", this.gameObject).GetComponent<Map>();
+
 		ScoreBoard.Instance.gameObject.SetActive(false);
-		StartTetris();
+		//StartTetris();
 	}
 
 	void StartTetris()
 	{
-		this.inputController = Spawner.SpawnObject("InputController", this.gameObject).GetComponent<InputController>();
 		this.player = Spawner.SpawnObject("Player", this.gameObject).GetComponent<Player>();
-		this.map = Spawner.SpawnObject("Map", this.gameObject).GetComponent<Map>();
-
 		this.inputController.AddKeyAction(new PlayerIngameKeyAction(player));
 		player.map = map;
-		ScoreBoard.Instance.Clear();
 		ScoreBoard.Instance.gameObject.SetActive(true);
+		ScoreBoard.Instance.Clear();
 	}
 }
---
+++ Assets/Scripts/UI/BlinkingText.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System;
+
+public class BlinkingText : MonoBehaviour {
+	void Update () {
+		float alpha = (DateTime.Now.Second % 2 == 0) ? 1f : 0f;
+		this.gameObject.GetComponent<CanvasRenderer>().SetAlpha(alpha);
+	}
+}
