84b0201fe4430befe4e54eca1728f74cfa5d4d7d
Masashi AKISUE <masashi.akisue@aktsk.jp>
2017-02-28 21:14:04 +0900

    add Next UI

---
+++ Assets/Scripts/UI/Next.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+
+public class Next : MonoBehaviour {
+	private static Next _instance;
+
+	public static Next Instance
+	{
+		get {
+			if (_instance == null)
+			{
+				_instance = (Next)FindObjectOfType(typeof(Next));
+			}
+
+			return _instance;
+		}
+	}
+
+	public void Awake()
+	{
+		if (Instance != this)
+		{
+			Destroy(gameObject);
+			return;
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+}
+
+}
