1a72e82b67786e28c2509c76379728cc4b5be3e5
Masashi AKISUE <masashi.akisue@aktsk.jp>
2017-03-01 19:29:20 +0900

    add super rotation logic

--- Assets/Scripts/Interface/ITurnable.cs
+++ Assets/Scripts/Interface/ITurnable.cs
@@ -1,13 +1,4 @@
-﻿using UnityEngine;
-using System.Collections.Generic;
-public interface ITurnable
-{
-	void TurnLeft();
-	void TurnRight();
-	List<Vector3> TurnedWorldPositions(TurnDirection direction);
-}
-
-public enum TurnDirection {
+﻿public enum TurnDirection {
 	Left,
 	Right
 }
